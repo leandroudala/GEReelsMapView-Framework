3de2046a81a33905ec4e878db6bc089a0554cea5
Leandro Udala <leandro.correia@radixeng.com.br>
2024-04-05 20:49:51 -0300

    Initial Commit

---
+++ GEReelsMapView/GEReelsMapView.h
@@ -0,0 +1,18 @@
+//
+//  GEReelsMapView.h
+//  GEReelsMapView
+//
+//  Created by Leandro Udala Correia da Silva on 05/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GEReelsMapView.
+FOUNDATION_EXPORT double GEReelsMapViewVersionNumber;
+
+//! Project version string for GEReelsMapView.
+FOUNDATION_EXPORT const unsigned char GEReelsMapViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GEReelsMapView/PublicHeader.h>
+
+
